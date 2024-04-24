e68d13ef06b9fcfb8b8abca1488de1a6eabc8bfe
Kahfi <kahfi@zero-one-group.com>
2024-04-24 09:52:56 +0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,58 @@
-use aggregator::*;
-use std::fmt::Display;
+use std::fs;
+use tokio_postgres::{Error, NoTls};
 
-fn main() {
-    let mut tweets: Vec<Tweet> = Vec::new();
+use clap::Parser;
 
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
-    let tweet2 = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
-
-    tweets.push(tweet);
-    tweets.push(tweet2);
-
-    notify(tweets);
-
-    let pair = Pair::new(10, 15);
-
-    pair.cmp_display();
-
-    // returns_summarizable(true);
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = String::from("postgres"))]
+    user: String,
+    #[arg(long, default_value_t = String::from("postgres"))]
+    password: String,
+    #[arg(long, default_value_t = String::from("54322"))]
+    port: String,
+    #[arg(long, default_value_t = String::from(""))]
+    file_path: String,
 }
 
-pub fn notify(items: Vec<Tweet>) {
-    for item in items {
-        println!("Breaking tweet ! {}", item.summarize());
-    }
-}
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let args = Args::parse();
+    let user = &args.user;
+    let password = &args.password;
+    let port = &args.port;
+    let file_path = &args.file_path;
 
-struct Pair<T> {
-    x: T,
-    y: T,
-}
+    let config = format!(
+        "host=localhost user={} port={} password={}",
+        user, port, password
+    );
+    let config_str = config.as_str();
 
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self { x, y }
-    }
-}
+    let (mut client, connection) = tokio_postgres::connect(config_str, NoTls).await?;
 
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
         }
+    });
+
+    println!("file: {}", file_path);
+
+    let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let prepared_statement = format!("BEGIN; {} ROLLBACK;", content);
+
+    let transaction = client.transaction().await?;
+
+    let result = transaction.batch_execute(&prepared_statement).await;
+
+    match result {
+        Ok(_) => println!("Successfully executed script"),
+        Err(err) => println!("Failed executing script: {}", err.to_string()),
     }
+
+    Ok(())
 }
